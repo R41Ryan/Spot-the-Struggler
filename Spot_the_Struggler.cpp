189c9ac4902a502dd2bf15d244d5f8a77f31d1d2
R41Ryan <ryanhuynh64@gmail.com>
2021-11-13 15:28:25 -0700

    Core SDL is initialized and the program is now able to open a window and close.

--- Spot_the_Struggler.cpp
+++ Spot_the_Struggler.cpp
@@ -2,6 +2,9 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <SDL_image.h>
+#include <map.h>
+#include <player.h>
+#include <character.h>
 
 const int SCREEN_WIDTH = 1200;
 const int SCREEN_HEIGHT = 800;
@@ -10,6 +13,9 @@ SDL_Window* gameWindow = NULL;
 SDL_Renderer* gameRenderer = NULL;
 
 TTF_Font* gameFont;
+
+bool keyStates[TOTAL_KEYS];
+bool mouseStates[TOTAL_MOUSE_BUTTONS];
 
 bool init() {
 	bool success = true;
@@ -64,4 +70,105 @@ bool init() {
 	}
 
 	return success;
+}
+
+void close()
+{
+	TTF_CloseFont(gameFont);
+	SDL_DestroyRenderer(gameRenderer);
+	SDL_DestroyWindow(gameWindow);
+	gameRenderer = NULL;
+	gameWindow = NULL;
+
+	IMG_Quit();
+	TTF_Quit();
+	SDL_Quit();
+}
+
+void setKeyState(SDL_Keycode sym, bool state)
+{
+	switch (sym)
+	{
+	case SDLK_w:
+		keyStates[KEY_W] = state;
+		break;
+	case SDLK_s:
+		keyStates[KEY_S] = state;
+		break;
+	case SDLK_a:
+		keyStates[KEY_A] = state;
+		break;
+	case SDLK_d:
+		keyStates[KEY_D] = state;
+		break;
+	case SDLK_p:
+		keyStates[KEY_P] = state;
+		break;
+	}
+}
+
+void setMouseState(SDL_MouseButtonEvent button, bool state)
+{
+	switch (button.button)
+	{
+	case SDL_BUTTON_LEFT:
+		mouseStates[LEFT_MOUSE_BUTTON] = state;
+		break;
+	case SDL_BUTTON_RIGHT:
+		mouseStates[RIGHT_MOUSE_BUTTON] = state;
+		break;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	srand(time(NULL)); // Initializes random number generator
+
+	for (int i = 0; i < TOTAL_KEYS; i++)
+	{
+		keyStates[i] = false;
+	}
+
+	if (!init())
+	{
+		printf("Failed to initialize.\n");
+	}
+	else
+	{
+		bool quit = false;
+
+		SDL_Event e;
+
+		while (!quit)
+		{
+			while (SDL_PollEvent(&e) > 0)
+			{
+				switch (e.type)
+				{
+				case SDL_QUIT:
+					quit = true;
+					break;
+				case SDL_KEYDOWN:
+					setKeyState(e.key.keysym.sym, true);
+					break;
+				case SDL_KEYUP:
+					setKeyState(e.key.keysym.sym, false);
+					break;
+				case SDL_MOUSEBUTTONDOWN:
+					setMouseState(e.button, true);
+					break;
+				case SDL_MOUSEBUTTONUP:
+					setMouseState(e.button, false);
+					break;
+				}
+			}
+
+			SDL_SetRenderDrawColor(gameRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+			SDL_RenderClear(gameRenderer);
+		}
+	}
+
+	close();
+
+	return 0;
 }
---
+++ header/player.h
@@ -0,0 +1,15 @@
+#pragma once
+enum inputKeys {
+	KEY_W,
+	KEY_S,
+	KEY_A,
+	KEY_D,
+	KEY_P,
+	TOTAL_KEYS
+};
+
+enum inputMouseButtons {
+	LEFT_MOUSE_BUTTON,
+	RIGHT_MOUSE_BUTTON,
+	TOTAL_MOUSE_BUTTONS
+};
